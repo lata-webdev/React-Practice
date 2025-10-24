94b782d6278cb46eeeddf1ad7c347f5043a6871b
lata <latakumari804419@gmail.com>
2025-10-24 23:08:10 +0530

    css add

---
+++ 05-css/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./Header.module.css";
+
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            <h1>lata</h1>
+            <button className='btn'>Login</button>
+        </div>
+    )
+}
+
+export default Header
