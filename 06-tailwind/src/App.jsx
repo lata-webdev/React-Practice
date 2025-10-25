68ff137a1ae35c118c50195217e4747ee4767c7a
lata <latakumari804419@gmail.com>
2025-10-25 16:25:07 +0530

    try-tailwind

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900'>
+      <h1 className='text-red-300 text-center text-8xl font-bold font-lg items-center justify-center flex h-screen'>I am using Tailwindcss here</h1>
+    </div>
+  )
+}
+
+export default App
