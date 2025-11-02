0b848be7a54703814ff3eb61c616c89fe3414705
lata <latakumari804419@gmail.com>
2025-11-02 20:00:29 +0530

    prevent default form submit

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+  const submitHandler = (e) => {
+    // console.log("form submitted"); mini sec ke liye show hoga ye message bcz bydefault ese hi rehta hai 
+    e.preventDefault();  // prevent bydefault  & show message now in console
+    console.log("Form Submited");
+  }
+  return (
+    <div>
+      <form onSubmit={(e) => {
+        submitHandler(e);
+      }}>
+        <input type="text" placeholder='Enter Your Name' />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
