ed43f2149c70183e9112bbfe6a693984c538b2a0
lata <latakumari804419@gmail.com>
2025-11-08 12:58:34 +0530

    UI added

--- 07-ui-project/src/App.jsx
+++ 07-ui-project/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import Section1 from './components/Section1/Section1'
-import Section2 from './components/Section2/Section2'
+import Section1 from "./components/Section1/Section1";
 import girl1 from "./assets/cardgirl-1.jpg";
 import girl2 from "./assets/cardgirl2.jpg";
 import girl3 from "./assets/cardgirl-3.jpg";
@@ -27,7 +26,6 @@ const App = () => {
   return (
     <div>
       <Section1 users={users} />
-      <Section2 />
     </div>
   )
 }
--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
+import Card from './components/Cards';
 
 const App = () => {
   const [userData, setUserdata] = useState([]);
@@ -31,8 +32,8 @@ const App = () => {
       <div className='flex flex-wrap gap-4 p-2'>
         {printUserdata}
       </div>
-      <div className='flex justify-center items-center p-4 gap-6'>
-        style={{ opacity: index == 1 ? 0.5 : 1 }}
+      <div className='flex justify-center items-center p-4 gap-6'
+       style={{ opacity: index == 1 ? 0.5 : 1 }}>
         <button className='bg-amber-400 text-black px-4 py-2 rounded font-semibold text-sm cursor-pointer active:scale-95'
           onClick={function () {
             console.log('Prev button Clicked');
